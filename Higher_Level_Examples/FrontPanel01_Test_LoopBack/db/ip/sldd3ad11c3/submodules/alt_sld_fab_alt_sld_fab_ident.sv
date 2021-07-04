dbbc240d88d9fce63fa0365d012b0581699150d9
Doug Gilliland <doug@douglasgilliland.com>
2021-07-04 00:59:15 -0400

    tested not working

--- Higher_Level_Examples/FrontPanel01_Test_LoopBack/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Higher_Level_Examples/FrontPanel01_Test_LoopBack/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 847dcc3294dc0a8189e3
+//   DESIGN_HASH   : a1e9cbdba57643b565a6
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h847dcc3294dc0a8189e3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha1e9cbdba57643b565a6 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
